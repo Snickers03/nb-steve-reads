289aac184df261fe2233cb2cd34347f02fce0618
= <niklas.bialk@gmail.com>
2023-05-04 21:45:50 +0200

    📝 add readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="pt-24">
+    <main className="pt-12">
       <div className="mx-auto w-2/3 p-6 bg-slate-300 rounded-md">
         <Header />
       </div>
